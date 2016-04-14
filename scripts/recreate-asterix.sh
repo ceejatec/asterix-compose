8bea3052288c859204ba358ea8fd1eb9d53b0618
Chris Hillery <ceej@couchbase.com>
2016-04-14 02:29:07 -0700

    Use absolute HOST paths for docker-compose.yml

--- scripts/recreate-asterix.sh
+++ scripts/recreate-asterix.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 scriptdir=`dirname $0`
-docker-compose -f ${scriptdir}/../app/docker-compose.yml up -d --force-recreate nc1 nc2 nc3 nc4
+# This path to docker-compose.yml must be be the ABSOLUTE path on the
+# DOCKER HOST, because docker is always invoked in the host context.
+docker-compose -f /home/couchbase/docker/app/docker-compose.yml up -d --force-recreate nc1 nc2 nc3 nc4
 ${scriptdir}/install-asterix.sh /home/couchbase/*.zip
 
--- scripts/recreate-couchbase.sh
+++ scripts/recreate-couchbase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 scriptdir=`dirname $0`
-docker-compose -f ${scriptdir}/../app/docker-compose.yml up -d --force-recreate couchbase1.host couchbase2.host couchbase3.host
+# This path to docker-compose.yml must be be the ABSOLUTE path on the
+# DOCKER HOST, because docker is always invoked in the host context.
+docker-compose -f /home/couchbase/docker/app/docker-compose.yml up -d --force-recreate couchbase1.host couchbase2.host couchbase3.host
 echo Sleeping until Couchbase initializes...
 sleep 7
 ${scriptdir}/config-couchbase.sh
