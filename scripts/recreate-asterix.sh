cb23593a2f7a526823dd3d91f3dc1643d5e694c4
Chris Hillery <ceej@couchbase.com>
2016-04-14 02:56:59 -0700

    Install arbitrary .zip

--- scripts/recreate-asterix.sh
+++ scripts/recreate-asterix.sh
@@ -4,5 +4,5 @@ scriptdir=`dirname $0`
 # This path to docker-compose.yml must be be the ABSOLUTE path on the
 # DOCKER HOST, because docker is always invoked in the host context.
 docker-compose -f /home/couchbase/docker/app/docker-compose.yml up -d --force-recreate nc1 nc2 nc3 nc4
-${scriptdir}/install-asterix.sh /home/couchbase/*.zip
+${scriptdir}/install-asterix.sh $*
 
