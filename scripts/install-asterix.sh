cf849781b49a71f5fe43439cd78f7b952c1f05e6
Chris Hillery <ceej@couchbase.com>
2016-04-14 03:37:28 -0700

    Ensure old Asterix installers are cleared out

--- scripts/install-asterix.sh
+++ scripts/install-asterix.sh
@@ -3,6 +3,7 @@
 # This script assumes the 4-node Docker cluster is up, and has not
 # previously had Asterix installed on it
 scriptdir=`dirname $0`
+rm -rf /home/couchbase/shared/*
 cp ${scriptdir}/docker-cluster.xml $* /home/couchbase/shared
 docker exec -u couchbase app_nc1_1 mkdir /home/couchbase/managix
 docker exec -u couchbase app_nc1_1 unzip -d /home/couchbase/managix /home/couchbase/shared/\*.zip
