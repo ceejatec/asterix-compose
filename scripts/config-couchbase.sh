e2ca791a4d076b300f4b24f19bb07a2f63c743db
Chris Hillery <ceej@couchbase.com>
2016-04-14 02:19:15 -0700

    Automate recreating Couchbase cluster

--- scripts/config-couchbase.sh
+++ scripts/config-couchbase.sh
@@ -5,18 +5,9 @@ docker run --rm --net=app_asterix couchbase \
   -c couchbase1.host \
   --cluster-init-username=Administrator \
   --cluster-init-password=couchbase \
-  --cluster-init-ramsize=600 \
+  --cluster-init-ramsize=1000 \
   -u admin -p password
-
-docker run --rm --net=app_asterix couchbase \
-  couchbase-cli bucket-create \
-  -c couchbase1.host \
-  --bucket=default \
-  --bucket-type=couchbase \
-  --bucket-port=11211 \
-  --bucket-ramsize=600 \
-  --bucket-replica=1 \
-  -u Administrator -p couchbase
+sleep 2
 
 docker run --rm --net=app_asterix couchbase \
   couchbase-cli server-add \
@@ -25,6 +16,7 @@ docker run --rm --net=app_asterix couchbase \
   --server-add couchbase2.host \
   --server-add-username Administrator \
   --server-add-password couchbase
+sleep 2
 
 docker run --rm --net=app_asterix couchbase \
   couchbase-cli rebalance \
@@ -33,5 +25,4 @@ docker run --rm --net=app_asterix couchbase \
   --server-add couchbase3.host \
   --server-add-username Administrator \
   --server-add-password couchbase
-
 
---
+++ scripts/recreate-couchbase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scriptdir=`dirname $0`
+docker-compose -f ${scriptdir}/../app/docker-compose.yml stop couchbase1.host couchbase2.host couchbase3.host || true
+rm -rf ${scriptdir}/../nodes/cb*
+docker-compose -f ${scriptdir}/../app/docker-compose.yml up -d --force-recreate couchbase1.host couchbase2.host couchbase3.host
+echo Sleeping until Couchbase initializes...
+sleep 7
+${scriptdir}/config-couchbase.sh
+
