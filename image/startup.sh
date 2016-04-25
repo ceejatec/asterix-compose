82a9f3094b614ef0afab1b82fe3de107cbb4c5d1
Chris Hillery <ceej@couchbase.com>
2016-04-25 02:12:20 -0700

    Set up Jenkins slave for external ssh

--- image/startup.sh
+++ image/startup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# First, copy any files in /ssh to /home/couchbase/.ssh, changing ownership to
+# user couchbase and maintaining permissions
+if [ -d /ssh ] && [ "$(ls -A /ssh)" ]
+then
+    cp -a /ssh/* /home/couchbase/.ssh
+fi
+cat /usr/sbin/sshconfigbase >> /home/couchbase/.ssh/config
+chown -R couchbase:couchbase /home/couchbase/.ssh
+chmod 600 /home/couchbase/.ssh/*
+
 # Start sshd (as new, long-running, foreground process)
 [[ "$1" == "default" ]] && {
     chown -R couchbase:couchbase /home/couchbase /home/couchbase
