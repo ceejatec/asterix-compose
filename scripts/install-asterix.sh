b4db057abee4c245e7e5f913b9f6bf9979ee35cf
Michael Blow <mblow@apache.org>
2016-07-19 18:43:58 -0400

    Use Ansible For AsterixDB Install / Deployment
    Removes use of managix / dependency on installer binary assembly

--- scripts/install-asterix.sh
+++ scripts/install-asterix.sh
@@ -3,12 +3,9 @@
 # This script assumes the 4-node Docker cluster is up, and has not
 # previously had Asterix installed on it
 scriptdir=`dirname $0`
-rm -rf /home/couchbase/shared/*
-cp ${scriptdir}/docker-cluster.xml $* /home/couchbase/shared
-docker exec -u couchbase app_nc1_1 mkdir /home/couchbase/managix
-docker exec -u couchbase app_nc1_1 unzip -d /home/couchbase/managix /home/couchbase/shared/\*.zip
-docker exec -u couchbase app_nc1_1 /home/couchbase/managix/bin/managix configure
-docker exec -u couchbase app_nc1_1 /home/couchbase/managix/bin/managix validate
-docker exec -u couchbase app_nc1_1 /home/couchbase/managix/bin/managix create -n docker-cluster -c /home/couchbase/shared/docker-cluster.xml 
+
+docker-compose /home/couchbase/docker/app/docker-compose.yml up -d --force-recreate ansible
+docker cp $* ansible:/home/couchbase/asterix-server-binary-assembly.zip
+docker exec -u couchbase ansible ansible-playbook /home/couchbase/ansible/playbook.yml -i /home/couchbase/ansible/inventory
 echo $?
 
